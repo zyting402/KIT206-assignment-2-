95b9f8425e4e798d5a130e4a6ba45888607cad3b
issac <lamch@utas.edu.au>
2021-12-04 19:36:03 +1100

    Create new program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KIT206_assignment_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
