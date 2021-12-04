e0346602c1c9ac64f828555218b3a761dff8a575
issac <lamch@utas.edu.au>
2021-12-04 23:28:06 +1100

    add student class

---
+++ Student.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206_assignment_2_
+{
+
+    class Student
+    {
+        public string familyName;
+        public string givenName;
+        public string title;
+        public string schoolUnit;
+        public string campus;
+        public string email;
+        public string jobTitle;
+        public DateTime commencedWithInst;
+        public DateTime commencedCurrentPos;
+
+        public float tenure;
+        public float publications;
+        public int id;
+        public Gender gender;
+
+        /*
+         * Task 2.2.3
+         */
+        public override string ToString()
+        {
+            return familyName + "\t" + id + "\t" + gender;
+        }
+
+    }
+}
